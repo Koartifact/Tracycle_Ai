9280544d3d44109e6e949ee07c15f6e356ecd255
Koartifact <kor7and3@korea.ac.kr>
2021-08-04 22:26:36 +0900

    파일 업데이트

--- flask_server.py
+++ flask_server.py
@@ -1,32 +1,58 @@
+import gdown
+from flask import Flask, render_template, request, redirect
+import torch
+from PIL import Image
+import os
+import io
+import argparse
 from flask import Flask, request, make_response, jsonify
 from flask_cors import CORS
-from img_classify import Target, Handler
+
 
 app = Flask(__name__)
+app.config['MAX_CONTENT_LENGTH'] = 50 * 1024 * 1024
 CORS(app)
 
-@app.route("/test", methods=['GET', 'POST', 'PUT', 'DELETE'])
-def test():
-    if request.method == 'POST':
-        print('POST')
-        data = request.get_json()
-        print(data)
-        print(data['email'])
-    if request.method == 'GET':
-        print('GET')
-        user = request.args.get('email')
-        print(user)
-    if request.method == 'PUT':
-        print('PUT')
-        user = request.args.get('email')
-        print(user)
-    if request.method == 'DELETE':
-        print('DELETE')
-        user = request.args.get('email')
-        print(user)
 
-    return make_response(jsonify({'status': True}), 200)
+google_path = 'https://drive.google.com/uc?id='
+file_id = '1SpZl2eB4zELOFmNBkObP1DAmTdmOWPZP'
+output_name = 'best.pt'
+gdown.download(google_path+file_id, output_name,quiet=False)
+
+@app.route("/", methods=["GET", "POST"])
+def predict():
+    if request.method == "POST":
+        if "file" not in request.files:
+            return redirect(request.url)
+        file = request.files["file"]
+        if not file:
+            return
+
+        img_bytes = file.read()
+        img = Image.open(io.BytesIO(img_bytes))
+        results = model(img, size=640)
+
+        # for debugging
+        # data = results.pandas().xyxy[0].to_json(orient="records")
+        # return data
+
+        results.render()  # updates results.imgs with boxes and labels
+        for img in results.imgs:
+            img_base64 = Image.fromarray(img)
+            img_base64.save("static/image0.jpg", format="JPEG")
+        return redirect("static/image0.jpg")
+
+    return render_template("index.html")
 
 
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port="8082")
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Flask app exposing yolov5 models")
+    parser.add_argument("--port", default=5000, type=int, help="port number")
+    args = parser.parse_args()
+
+    model = torch.hub.load(
+        'ultralytics/yolov5', 'custom', path='best.pt'
+    ).autoshape()  # force_reload = recache latest code
+    model.eval()
+    app.run(host="0.0.0.0", port=args.port)  # debug=True causes Restarting with stat
