0122ac5657e1bb74b53ae51fe9c26cef86fe883d
Koartifact <kor7and3@korea.ac.kr>
2021-08-09 21:24:15 +0900

    빠꾸

--- flask_server.py
+++ flask_server.py
@@ -8,61 +8,17 @@ from flask_cors import CORS
 import pymysql
 import dbconfig
 import json
-############  파일 감시 추가
-import os
-import time
-from watchdog.observers import Observer
-from watchdog.events import FileSystemEventHandler
 
+app = Flask(__name__)
+app.config['MAX_CONTENT_LENGTH'] = 50 * 1024 * 1024
+CORS(app)
 
-
-#### 구글 드라이브에서 가중치 파일 받는 것 추가 테스트 땐 한번 받고나면 주석처리
+###구글 드라이브에서 가중치 파일 받는 것 추가 테스트 땐 한번 받고 주석처리
 # google_path = 'https://drive.google.com/uc?id='
 # file_id = '1-bEBxnujEU-R-p29-QM8eFFeRICwjYey'
 # output_name = 'best.pt'
 # gdown.download(google_path+file_id, output_name,quiet=False)
-####
-
-
-#### 파일 감시부분 ###########################################
-class Target:
-    watchDir = os.getcwd()
-    #watchDir에 감시하려는 디렉토리를 명시한다.
-
-    def __init__(self):
-        self.observer = Observer()  # observer객체를 만듦
-
-    def run(self):
-        event_handler = Handler()
-        self.observer.schedule(event_handler, self.watchDir,
-                               recursive=True)
-        self.observer.start()
-        try:
-            while True:
-                time.sleep(1)
-        except:
-            self.observer.stop()
-            print("Error")
-            self.observer.join()
-
-class Handler(FileSystemEventHandler):
-    #FileSystemEventHandler 클래스를 상속받음.
-    #아래 핸들러들을 오버라이드 함
-
-    def on_created(self, event): #파일, 디렉터리가 생성되면 실행
-        생성되면 result0 을 result1로 바꿈 바꿀때 덮어씌움 그럼 0은 다시 만들기 전까지 없어지는 것 만들기
-
-# def on_modified(self, event): #파일, 디렉터리가 수정되면 실행
-
-    
-#############################################################
-
-
-
-#### flask 서버 시작 #####################
-app = Flask(__name__)
-app.config['MAX_CONTENT_LENGTH'] = 50 * 1024 * 1024
-CORS(app)
+####e
 
 
 def get_result(dbname, areaId, categoryId):
@@ -99,7 +55,7 @@ def predict():
         file = request.files["mainFile"]
         print(file)
 
-    ########################################################################
+    #########################################################################
      # yolo에서 보내주는 값 json 으로 받음 기본설정이라 안건드림
         img_bytes = file.read()
         img = Image.open(io.BytesIO(img_bytes))
@@ -126,7 +82,7 @@ def predict():
             infos = "Can't find object"
 
         else:
-         
+         # db 찾아서 출력 어떻게 띄우지
             for c in class_name:
                 categoryId = c
                 print(categoryId)
@@ -135,14 +91,14 @@ def predict():
 
     return jsonify(infos)
 
-################################################
+
 @app.route("/img", methods=["GET", "POST"])
 def show():
     if request.method == 'GET':
         print('GET')
-        link = "static/result1.jpg" # result0이 안만들어지거나 없으면 1이 안생김
+        link = "static/result0.jpg"
+        print(link)
     return jsonify(link)
-################################################ 
 
 
 if __name__ == "__main__":
@@ -155,6 +111,3 @@ if __name__ == "__main__":
     model.eval()
     # debug=True causes Restarting with stat
     app.run(host="0.0.0.0", port=args.port)
-
-    w = Target("static/")
-    w.run()
